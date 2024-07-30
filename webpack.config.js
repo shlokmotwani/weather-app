1776ee5de44c5493fb2922d6a2451baad668a17f
Shlok Motwani <shlok.codes@gmail.com>
2024-07-30 16:10:34 +0530

    Initial file setup commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "none",
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: "body",
+      title: "Weather App",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
