118fd9f4fae164c30650436aa2f39fe22f2602b2
Shlok Motwani <shlok.codes@gmail.com>
2024-08-02 20:11:34 +0530

    feat: top div UI improved

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import './style.css';
+import "./style.css";
 
 const URL_LEFT =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 const URL_RIGHT =
   "?unitGroup=metric&key=HRAC76VQCCEJMMRPLCSUF98AF&contentType=json";
-const inputField = document.querySelector("#location-input");
-const fetchButton = document.querySelector("#fetch-button");
-const contentDiv = document.querySelector("#content-div");
+
 let weatherReportJSON;
 let weatherReportJSONExtracted = {};
 
-async function getWeatherReport() {
-  const location = inputField.value;
+async function getWeatherReport(location) {
   const url = URL_LEFT + location + URL_RIGHT;
   let result = await fetch(url, { mode: "cors" });
   weatherReportJSON = await result.json();
@@ -20,8 +17,10 @@ async function getWeatherReport() {
 function extractNecessaryDetails() {
   weatherReportJSONExtracted.resolvedAddress =
     weatherReportJSON.resolvedAddress;
+  weatherReportJSONExtracted.timezone = weatherReportJSON.timezone;
   weatherReportJSONExtracted.conditions =
     weatherReportJSON.currentConditions.conditions;
+  weatherReportJSONExtracted.icon = weatherReportJSON.currentConditions.icon;
   weatherReportJSONExtracted.datetime =
     weatherReportJSON.currentConditions.datetime;
   weatherReportJSONExtracted.dew = weatherReportJSON.currentConditions.dew;
@@ -34,48 +33,135 @@ function extractNecessaryDetails() {
   weatherReportJSONExtracted.temp = weatherReportJSON.currentConditions.temp;
   weatherReportJSONExtracted.windspeed =
     weatherReportJSON.currentConditions.windspeed;
+  weatherReportJSONExtracted.uvindex =
+    weatherReportJSON.currentConditions.uvindex;
+  weatherReportJSONExtracted.sunrise =
+    weatherReportJSON.currentConditions.sunrise;
+  weatherReportJSONExtracted.sunset =
+    weatherReportJSON.currentConditions.sunset;
 }
 
 function loadWeatherCard() {
-  let weatherCard = document.createElement("div");
-  let address = document.createElement("div");
-  let conditions = document.createElement("div");
-  let datetime = document.createElement("div");
-  let dew = document.createElement("div");
-  let feelslike = document.createElement("div");
-  let humidity = document.createElement("div");
-  let pressure = document.createElement("div");
-  let temp = document.createElement("div");
-  let windspeed = document.createElement("div");
-
-  address.textContent = weatherReportJSONExtracted.resolvedAddress;
-  conditions.textContent = weatherReportJSONExtracted.conditions;
-  datetime.textContent = weatherReportJSONExtracted.datetime;
-  dew.textContent = weatherReportJSONExtracted.dew;
-  feelslike.textContent = weatherReportJSONExtracted.feelslike;
-  humidity.textContent = weatherReportJSONExtracted.humidity;
-  pressure.textContent = weatherReportJSONExtracted.pressure;
-  temp.textContent = weatherReportJSONExtracted.temp;
-  windspeed.textContent = weatherReportJSONExtracted.windspeed;
-
-  weatherCard.appendChild(address);
-  weatherCard.appendChild(conditions);
-  weatherCard.appendChild(datetime);
-  weatherCard.appendChild(dew);
-  weatherCard.appendChild(feelslike);
-  weatherCard.appendChild(humidity);
-  weatherCard.appendChild(pressure);
-  weatherCard.appendChild(temp);
-  weatherCard.appendChild(windspeed);
-
-  weatherCard.style.backgroundColor = "rgba(100, 100, 100, 0.8)";
-  contentDiv.appendChild(weatherCard);
+  loadTopLeftCard();
+  loadTopRightCard();
 }
 
-// fetchButton.addEventListener("click", async (event) => {
-//   contentDiv.innerHTML = "";
-//   await getWeatherReport();
-//   extractNecessaryDetails();
-//   loadWeatherCard();
-//   event.preventDefault();
-// });
+function loadTopLeftCard() {
+  let topLeftDiv = document.querySelector("#top-left");
+  topLeftDiv.innerHTML = "";
+
+  let itemsData = [
+    {
+      value: weatherReportJSONExtracted.conditions,
+    },
+    {
+      value: weatherReportJSONExtracted.icon,
+    },
+    {
+      value: weatherReportJSONExtracted.temp,
+    },
+    {
+      value: weatherReportJSONExtracted.resolvedAddress,
+    },
+    {
+      value: weatherReportJSONExtracted.datetime,
+    },
+  ];
+
+  itemsData.forEach((item) => {
+    let itemDiv = document.createElement("div");
+    itemDiv.textContent = item.value;
+
+    itemDiv.style.cssText = `
+    border: 1px solid white;
+    text-align: center;
+    padding: 10px;
+    width: 100%;
+    `;
+
+    topLeftDiv.appendChild(itemDiv);
+  });
+
+  let location = document.createElement("input");
+  location.placeholder = "Location";
+  location.style.cssText = `
+  color: black;
+  padding: 3px;
+  font-size: 1.0rem;
+  border: 1px solid white;
+  text-align: center;
+  padding: 10px;
+  width: 100%;`;
+
+  location.addEventListener("keypress", async (event) => {
+    if (event.key == "Enter") {
+      fetchAndDisplayWeatherReport(location.value);
+    }
+  });
+
+  topLeftDiv.appendChild(location);
+}
+
+function loadTopRightCard() {
+  let topRightDiv = document.querySelector("#top-right");
+  topRightDiv.innerHTML = "";
+
+  let itemsData = [
+    {
+      text: "Feels like",
+      value: weatherReportJSONExtracted.feelslike,
+    },
+    {
+      text: "Humidity",
+      value: weatherReportJSONExtracted.humidity,
+    },
+    {
+      text: "UV Index",
+      value: weatherReportJSONExtracted.uvindex,
+    },
+    {
+      text: "Wind Speed",
+      value: weatherReportJSONExtracted.windspeed,
+    },
+    {
+      text: "Sunrise",
+      value: weatherReportJSONExtracted.sunrise,
+    },
+    {
+      text: "Sunset",
+      value: weatherReportJSONExtracted.sunset,
+    },
+  ];
+
+  itemsData.forEach((item) => {
+    let itemDiv = document.createElement("div");
+    let itemText = document.createElement("span");
+    let itemValue = document.createElement("div");
+
+    itemDiv.style.cssText = `
+    display: flex;
+    border: 1px solid white;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px;
+    width: 100%;
+    `;
+
+    itemDiv.appendChild(itemText);
+    itemDiv.appendChild(itemValue);
+
+    itemText.textContent = item.text;
+    itemValue.textContent = item.value;
+
+    topRightDiv.appendChild(itemDiv);
+  });
+}
+
+async function fetchAndDisplayWeatherReport(location) {
+  await getWeatherReport(location);
+  console.log(weatherReportJSON);
+  extractNecessaryDetails();
+  loadWeatherCard();
+}
+
+fetchAndDisplayWeatherReport("Bilaspur");
